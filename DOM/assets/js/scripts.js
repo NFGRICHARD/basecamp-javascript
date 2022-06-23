3021afcf1e1d5371f23dcd67c34ae872265b6e9f
Richard Brederode <richardbrederode@hotmail.com>
2022-06-23 08:29:08 -0300

    atividade

--- DOM/assets/js/scripts.js
+++ DOM/assets/js/scripts.js
@@ -1,33 +1,31 @@
-function changeMode() {
-	changeClasses();
-	changeText();
+const h1 = document.getElementById("page-title")
+const button = document.getElementById("mode-selector")
+const footer = document.getElementsByTagName('footer')[0]
+const body = document.getElementsByTagName('body')[0]
+const darkModeClass = 'dark-mode'
+
+button.addEventListener("click", botao)
+function botao(){
+    mudaclasses();
+    mudanome();
 }
 
-function changeClasses() {
-	button.classList.toggle(darkModeClass);
-	h1.classList.toggle(darkModeClass);
-	body.classList.toggle(darkModeClass);
-	footer.classList.toggle(darkModeClass);
+function mudaclasses (){
+    h1.classList.toggle(darkModeClass)
+    body.classList.toggle(darkModeClass)
+    button.classList.toggle(darkModeClass)
+    footer.classList.toggle(darkModeClass)
 }
+function mudanome(){
+const darkMode = 'Dark Mode'
+const lightMode = 'Light Mode'
 
-function changeText() {
-	const lightMode = 'Light Mode';
-	const darkMode = 'Dark Mode';
+    if(body.classList.contains(darkModeClass)/* AQUI TA verificando se a classe darkModeClass ta adicionada*/){
+        button.innerHTML = lightMode //se tiver o nome do botão va ser 'Light Mode
+        h1.innerHTML = darkMode + ' ON' // e o nome do texto vai muda
+        return
+    }
+    button.innerHTML = darkMode 
+    h1.innerHTML = lightMode + ' ON'
 
-	if (body.classList.contains(darkModeClass)) {
-		button.innerHTML = lightMode;
-		h1.innerHTML = darkMode + ' ON';
-		return;
-	}
-
-	button.innerHTML = darkMode;
-	h1.innerHTML = lightMode + ' ON';
 }
-
-const darkModeClass = 'dark-mode';
-const button = document.getElementById('mode-selector');
-const h1 = document.getElementById('page-title');
-const body = document.getElementsByTagName('body')[0];
-const footer = document.getElementsByTagName('footer')[0];
-
-button.addEventListener('click', changeMode);
--- orientacao-a-objetos/ContaBancaria.js
+++ orientacao-a-objetos/ContaBancaria.js
@@ -1,72 +1,73 @@
+/*Nesta atividade, vamos testar os conceitos de Orientação a Objetos simulando a criação de diversos tipos de contas bancárias e operações disponíveis em cada uma.
+
+ok Crie a classe ContaBancaria, que possui os parâmetros agencia, numero, tipo e saldo;
+ok Dentro de ContaBancaria, construa o getter e o setter de saldo;
+od Dentro de ContaBancaria, crie os métodos sacar e depositar;
+ok Crie uma classe-filha chamada ContaCorrente que herda todos esses parâmetros e ainda possua o parâmetro  cartaoCredito;
+ok Ainda em ContaCorrente, construa o getter e o setter de cartaoCredito;
+ok Ainda em ContaCorrente, faça com que o tipo seja 'conta corrente' por padrão;
+ok Crie uma classe-filha chamada ContaPoupanca que herda todos os parâmetros de ContaBancaria;
+ok Crie uma classe-filha chamada ContaUniversitaria que herda todos os parâmetros de ContaBancaria;
+okFaça com que o método saque de ContaUniversitaria apenas seja capaz de sacar valores menores que 500 reais.*/
+
 class ContaBancaria {
-	constructor(agencia, numero, tipo) {
-		this.agencia = agencia;
-		this.numero = numero;
-		this.tipo = tipo;
-		this._saldo = 0;
-	}
+    constructor (agencia, numero, tipo){
+        this.agencia = agencia
+        this.numero = numero
+        this.tipo = tipo
+        this._saldo = 0   
+    }
+    set saldo(saldoDaConta){
+        this._saldo = saldoDaConta
+    }
+    get saldo(){
+        return this._saldo
+    }
+    sacar(valorDeSaque){
+        if(valorDeSaque > this._saldo){
+            return  'Saldo insuficiente'
+        }
+        this._saldo = this._saldo - valorDeSaque
+        return this._saldo
+    }
+    deposito(deposito){
+        this._saldo = this._saldo - deposito
+        return this._saldo
+    }
 
-	sacar(valor) {
-		if (valor > this._saldo) {
-			return console.log('Saque negado; saldo insuficiente!');
-		}
-
-		this._saldo = this._saldo - valor;
-		return this._saldo;
-	}
-
-	depositar(valor) {
-		this._saldo = this._saldo + valor;
-		return this._saldo;
-	}
-
-	set saldo(valor) {
-		this._saldo = valor;
-	}
-
-	get saldo() {
-		return this._saldo;
-	}
 }
 
-class ContaCorrente extends ContaBancaria {
-	constructor(agencia, numero, cartaoCredito) {
-		super(agencia, numero);
-		this.tipo = 'corrente';
-		this._cartaoCredito = cartaoCredito;
-	}
-
-	set cartaoCredito(valor) {
-		this._cartaoCredito = valor;
-	}
-
-	get cartaoCredito() {
-		return this._cartaoCredito;
-	}
+class ContaCorrente extends ContaBancaria{
+    constructor(agencia, numero, cartaoCredito){
+        super(agencia, numero)
+        this.tipo = 'Conta corrente'
+        this.cartaoCredito = cartaoCredito
+    }
+    set cartaoCredito(valor){
+        this._cartaoCredito = valor
+    }
+    get cartaoCredito(){
+        return this._cartaoCredito
+    }
 }
 
-class ContaPoupanca extends ContaBancaria {
-	constructor(agencia, numero) {
-		super(agencia, numero);
-		this.tipo = 'poupança';
-	}
+class ContaPoupanca extends ContaBancaria{
+    constructor(agencia, numero){
+        super(agencia, numero)
+        this.tipo = 'Conta Poupaca'
+    }
+
 }
 
-class ContaUniversitaria extends ContaBancaria {
-	constructor(agencia, numero) {
-		super(agencia, numero);
-		this.tipo = 'universitária';
-	}
-
-	sacar(valor) {
-		if (valor > 500) {
-			return 'Operação negada.';
-		}
-
-		this._saldo = this._saldo - valor;
-		return this._saldo;
-	}
+class ContaUniversitaria extends ContaBancaria{
+    constructor(agencia, numero, tipo){
+        super(agencia, numero, tipo)
+    }
+    sacar(sacarDeConta){
+        if(sacarDeConta > 500){
+            return 'Operação negada!'
+        }
+        this._saldo = this._saldo - sacarDeConta
+        return this._saldo
+    }
 }
-
-const minhaConta = new ContaCorrente(1, 211, true);
-const contaUni = new ContaUniversitaria(2, 333);
